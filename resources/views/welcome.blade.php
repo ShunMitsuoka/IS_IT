76aabda10e36dac22d3b65c0c25f567795833396
ShunMitsuoka <shun.iphone.app@gmail.com>
2020-08-01 19:32:47 +0900

    タイトル変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    光岡駿
                 </div>
 
                 <div class="links">
